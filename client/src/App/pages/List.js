32de9dd06656884804c715d06fd5059288efd395
horusspade <eshane.j@gmail.com>
2018-11-24 01:42:55 -0400

    got api working on server and client
    Api on server and client working. Will start building my own api from this

--- client/src/App/pages/List.js
+++ client/src/App/pages/List.js
@@ -5,27 +5,58 @@ class List extends Component {
   constructor(props){
     super(props);
     this.state = {
-      list: []
+        list: [],
+        summoner: {},
+        match: {}
     }
   }
 
   // Fetch the list on first mount
   componentDidMount() {
-    this.getList();
+      this.getList();
+      this.getSummoner();
+      this.getMatch();
   }
 
   // Retrieves the list of items from the Express app
+    getMatch = () => {
+        fetch('/api/lolm')
+            .then(res => res.json())
+            .then(match => this.setState({ match }))
+
+    }
+    getSummoner = () => {
+        fetch('/api/lol')
+            .then(res => res.json())
+            .then(summoner => this.setState({ summoner }))
+
+    }
   getList = () => {
     fetch('/api/getList')
     .then(res => res.json())
-    .then(list => this.setState({ list }))
-  }
+          .then(list => this.setState({ list }))
+        
+    }
+
+    
+
 
   render() {
-    const { list } = this.state;
+      const { list } = this.state;
+      const { summoner } = this.state;
+      const { match } = this.state;
 
-    return (
-      <div className="App">
+      return (
+          <div className="App">
+              <h1>{summoner.name}</h1>
+              <h1>{summoner.accountId}</h1>
+              <h1>{match.lane}
+
+              </h1>
+        
+
+
+
         <h1>List of Items</h1>
         {/* Check to see if any items are found*/}
         {list.length ? (
--- index.js
+++ index.js
@@ -1,21 +1,55 @@
 const express = require('express');
 const path = require('path');
+const lol = "https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/soloqdemon?api_key=RGAPI-921eeea1-1294-4005-81cb-b45e25945622";
+const lolm = "https://na1.api.riotgames.com/lol/match/v3/matchlists/by-account/70793?api_key=RGAPI-921eeea1-1294-4005-81cb-b45e25945622";
 
 const app = express();
+const fetch = require('node-fetch');
+const bodyParser = require('body-parser')
+
+loldata = {};
+loldatamatch = {};
+request = require('request');
 
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
-
+app.use(bodyParser.urlencoded({ extended: false }));
 // An api endpoint that returns a short list of items
 app.get('/api/getList', (req,res) => {
-	var list = ["item1", "item2", "item3"];
-	res.json(list);
-	console.log('Sent list of items');
+    var list = ["item781", "item2", "item3"];
+    res.json(list);
+
+    });
+
+// app.get('/api/lol', (req,res) => {
+// fetch(lol)
+//   .then(response => response.json())
+//         .then(data => console.log(data))
+
+
+//});
+app.get('/api/lol', (req, res) => {
+    var data = loldata;
+    res.json(data);
+    console.log("lol data sent")
 });
+app.get('/api/lolm', (req, res) => {
+    var data = loldatamatch;
+    res.json(data.matches[0]);
+    console.log("lol datamatch sent")
+});
+
+fetch(lolm)
+    .then(response => response.json())
+    .then(data => console.log(loldatamatch = data))
+
+fetch(lol)
+    .then(response => response.json())
+    .then(data => loldata = data)
 
 // Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
-	res.sendFile(path.join(__dirname+'/client/build/index.html'));
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
 const port = process.env.PORT || 5000;
